481716fd8647cc3f83273e145283e3f7037f6f69
ZecarlosF07 <167797727+ZecarlosF07@users.noreply.github.com>
2025-09-23 11:04:09 -0500

    prueba 2 carr

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import { Button } from "@/components/ui/button";
 import { ArrowRight, Award, Users, TrendingUp, ChevronLeft, ChevronRight } from "lucide-react";
 import heroImage from "@/assets/hero-image.jpg";
-import caruImage from "@/assets/luren_manos.png";
+import caruImage from "@/assets/luren-manos.png";
 
 const Hero = () => {
   const [currentSlide, setCurrentSlide] = useState(0);
